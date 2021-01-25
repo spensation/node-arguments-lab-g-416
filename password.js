ddad8ea5ed13835ea05ec2931fef2243e3b8b134
Spencer Evans <spencevans@gmail.com>
2021-01-25 23:09:43 +0000

    Automatically backed up by Learn

--- password.js
+++ password.js
@@ -1,3 +1,11 @@
 function password(len, num) {
-  const charset = ""
+  const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_*&%$!";
+  let retVal = "";
+  
+  for (let i = 0; i < num; i++) {
+    for let j = 0; j < len; j++) {
+      
+    }
+    console.log(retVal);
+  }
 } 
