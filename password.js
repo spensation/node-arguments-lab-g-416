05812936a2ae8c4c950fdd5a1ef604cafddfcfeb
Spencer Evans <spencevans@gmail.com>
2021-01-25 23:15:06 +0000

    Automatically backed up by Learn

--- password.js
+++ password.js
@@ -1,8 +1,9 @@
 function password(len, num) {
   const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_*&%$!";
-  let retVal = "";
+  
   
   for (let i = 0; i < num; i++) {
+    let retVal = "";
     for (let j = 0, n = charset.length; j < len; j++) {
       retVal += charset.charAt(Math.floor(Math.random() * n ));  
     }
